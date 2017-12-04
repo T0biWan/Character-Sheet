4ea72233d6a2ae55cee1060603cde05c64cf0829
tobi <tobi.klatt@web.de>
2017-12-04 23:29:23 +0100

    implemented tiles and boxes for values

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="app">
     <hero title="Heldendokument"></hero>
+    <div <div class="container">
+      <section>
+        <div class="tile is-ancestor">
+          <tile><box title="MU" value="10"></box></tile>
+          <tile><box title="KL" value="10"></box></tile>
+          <tile><box title="IN" value="10"></box></tile>
+          <tile><box title="CH" value="10"></box></tile>
+          <tile><box title="FF" value="10"></box></tile>
+          <tile><box title="GE" value="10"></box></tile>
+          <tile><box title="KO" value="10"></box></tile>
+          <tile><box title="KK" value="10"></box></tile>
+        </div>
+
+      </section>
+    </div>
+
   </div>
 </template>
 
 <script>
 import hero from "./components/hero.vue"
-import eigenschaft from "./components/eigenschaft.vue"
-import personal_data from "./components/personal_data.vue"
+import box from "./components/box.vue"
+import tile from "./components/tile.vue"
 
 export default {
   name: 'app',
   components: {
     'hero': hero,
-    'eigenschaft': eigenschaft,
-    'personal-data': personal_data,
+    'box': box,
+    'tile': tile,
+
   }
 }
 </script>
