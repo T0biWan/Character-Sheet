9c24465fc6c1d7fae69a67f9ab3931ce8af78f10
tobi <tobi.klatt@web.de>
2017-12-04 23:29:01 +0100

    added components

---
+++ src/components/box.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box has-text-centered">
+    <h2 class="title is-4">{{title}}</h2>
+    <p class="">{{value}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "box",
+    props: {
+      title: this.title,
+      value: this.value
+    },
+  }
+</script>
---
+++ src/components/tile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tile is-parent">
+    <div class="tile is-child">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "tile",
+  }
+</script>
